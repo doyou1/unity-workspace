dc6dff8ae40d71946695b7a407deed1d32260f52
doyou1 <tla149@daum.net>
2023-06-29 07:39:20 +0900

    add moving platform event

---
+++ unity-sampling/2D-Game-Sampling/Assets/Scripts/Sticky Platform.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyPlatform : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log("213");
+        if(collision.gameObject.name == "Player") {
+            collision.gameObject.transform.SetParent(transform);
+        }
+    }
+
+    private void OnTriggerExit2D(Collision2D collision) {
+        if(collision.gameObject.name == "Player") {
+            collision.gameObject.transform.SetParent(null);
+        }
+    }
+}
