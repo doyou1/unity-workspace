dc32341d561e589c78d31405a1827973679eb958
doyou1 <tla149@daum.net>
2023-07-09 09:56:41 +0900

    complete

--- unity-sampling/ColorSwitchReplica(Brackeys)/Assets/Scripts/Rotator.cs
+++ unity-sampling/ColorSwitchReplica(Brackeys)/Assets/Scripts/Rotator.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 100f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0f,0f, speed * Time.deltaTime);    
     }
 }
